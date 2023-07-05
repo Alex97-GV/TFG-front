5475ca3339ff5536bd6af90d45c8d7ca9a539eb6
Alex97-GV <alexmilangv@gmail.com>
2023-07-05 21:03:05 +0200

    generación de la vista signup y su formulario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { PassResetComponent } from './pass-reset/pass-reset.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LogInComponent},  
-  {path: 'pass-reset', component: PassResetComponent},  
+  {path: 'pass-reset', component: PassResetComponent},
+  {path: 'signup', component: SignupComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
 
